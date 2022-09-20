3f01a7d255e3e091ec8d49c940e06357e615703e
ezell38 <ezell38@charlottesvilleschools.org>
2022-09-20 13:22:55 -0400

    Crash_2

---
+++ raspberry-pi/Crash_2.py
@@ -0,0 +1,39 @@
+# Elias Zell
+# Crash 2
+
+import time 
+import digitalio  
+import board 
+import adafruit_mpu6050
+import busio
+
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(scl_pin, sda_pin)
+led=digitalio.DigitalInOut(board.GP18)
+
+mpu = adafruit_mpu6050.MPU6050(i2c) 
+
+led.direction=digitalio.Direction.OUTPUT
+
+mpu.acceleration 
+mpu.gyro
+
+
+
+while True: 
+
+    print(f"X Acceleration: {mpu.acceleration[0]} m/s2")
+    print(f"Y Acceleration: {mpu.acceleration[1]} m/s2")
+    print(f"Z Acceleration: {mpu.acceleration[2]} m/s2")
+    print("") 
+    
+    print("")
+    time.sleep(.5)
+
+    if mpu.acceleration[0]>9 or mpu.acceleration[1]>9 or mpu.acceleration[0]<-9 or mpu.acceleration[1]<-9:
+    
+        led.value=True 
+
+    else:
+        led.value=False    
