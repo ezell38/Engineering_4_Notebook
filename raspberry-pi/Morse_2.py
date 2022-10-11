bb88f5804f4b6704e0243bc05525eae1ee6b4ecc
ezell38 <ezell38@charlottesvilleschools.org>
2022-10-11 14:00:10 -0400

    hstfgjhgdfjh

---
+++ raspberry-pi/Morse_2.py
@@ -0,0 +1,65 @@
+import board 
+import time 
+import digitalio  
+
+led=digitalio.DigitalInOut(board.GP18)
+
+
+led.direction=digitalio.Direction.OUTPUT
+
+
+MORSE_CODE = { ' ':'/', 'A':'.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+    '?':'..--..', '/':'-..-.', '-':'-....-',
+    '(':'-.--.', ')':'-.--.-'}
+
+modifier = 0.25
+dot_time = 1*modifier
+dash_time = 3*modifier
+between_taps = 1*modifier
+between_letters = 3*modifier
+between_words = 7*modifier
+
+
+while True:
+    message = input("Enter Morse Code Message, or enter -q to quit: ")
+    
+    
+    if message == "-q":
+      break
+
+    message = message.upper()
+    umym = ""
+
+    for letter in message:
+      umym = umym + MORSE_CODE[letter] + " "
+
+    print(umym)
+
+    for character in umym:
+      if character == ".":
+        led.value=True
+        time.sleep(dot_time)
+        led.value=False
+        time.sleep(between_taps)
+
+
+      if character == "-":
+        led.value=True
+        time.sleep(dash_time)
+        led.value=False
+        time.sleep(between_taps)
+
+      if character == " ":
+        time.sleep(between_words)
